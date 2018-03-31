1fb795ed988edc26cac951bb706e8302d1499ff0
Brian <bdburns6389@gmail.com>
2018-03-30 21:56:15 -0500

    ViewRecords in RecordController needs work to populate html template

--- WorkoutGenerator/ViewModels/ViewRecordViewModel.cs
+++ WorkoutGenerator/ViewModels/ViewRecordViewModel.cs
@@ -2,10 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WorkoutGenerator.Models;
 
 namespace WorkoutGenerator.ViewModels
 {
     public class ViewRecordViewModel
     {
+        public Workout Workout { get; set; }
+        public IList<ExerciseWorkout> Exercises { get; set; }
+        public IList<Record> Records { get; set; }
     }
 }
