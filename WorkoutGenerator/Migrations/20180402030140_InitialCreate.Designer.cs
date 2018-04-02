244caf89d6f31498725ba154f2cd15674980e0fe
Brian <bdburns6389@gmail.com>
2018-04-01 22:05:37 -0500

    Redid database

--- WorkoutGenerator/Migrations/20180402030140_InitialCreate.Designer.cs
+++ WorkoutGenerator/Migrations/20180402030140_InitialCreate.Designer.cs
@@ -11,7 +11,7 @@ using WorkoutGenerator.Data;
 namespace WorkoutGenerator.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20180327170912_InitialCreate")]
+    [Migration("20180402030140_InitialCreate")]
     partial class InitialCreate
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
