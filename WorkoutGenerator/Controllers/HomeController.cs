d2d13550c5e5c1717f96edd8d735139caea67876
Brian <bdburns6389@gmail.com>
2018-04-10 14:03:59 -0500

    Added action to home controller for google maps, as well as HTML Template.  Look up API for Google Maps

--- WorkoutGenerator/Controllers/HomeController.cs
+++ WorkoutGenerator/Controllers/HomeController.cs
@@ -35,5 +35,10 @@ namespace WorkoutGenerator.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult GymLocations()
+        {
+            return View();
+        }
     }
 }
