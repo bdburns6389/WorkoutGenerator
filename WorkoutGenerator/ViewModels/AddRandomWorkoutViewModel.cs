8592c25c9b77ff1ccf730f79add7ea208cc986ab
Brian <bdburns6389@gmail.com>
2018-04-19 08:15:21 -0500

    Changed display name in Random.html

--- WorkoutGenerator/ViewModels/AddRandomWorkoutViewModel.cs
+++ WorkoutGenerator/ViewModels/AddRandomWorkoutViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +8,7 @@ namespace WorkoutGenerator.ViewModels
 {
     public class AddRandomWorkoutViewModel
     {
+        [Display(Name = "Choose a Name for your workout")]
         public string Name { get; set; }
 
         public AddRandomWorkoutViewModel()
