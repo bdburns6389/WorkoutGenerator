3fe0ca7f174a776b75f73c68df93a0afac4b5027
bdburns6389 <bdburns6389@gmail.com>
2018-05-19 12:13:38 -0500

    Working on RDS connection to database on AWS. Helpers.GetRDSConnectionString() is not working properly

---
+++ WorkoutGenerator/Data/RDSContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+namespace WorkoutGenerator.Models
+{
+    public class RDSContext : DbContext
+    {
+        public RDSContext()
+          : base(Helpers.GetRDSConnectionString())
+        {
+        }
+
+        public static RDSContext Create()
+        {
+            return new RDSContext();
+        }
+    }
+}
